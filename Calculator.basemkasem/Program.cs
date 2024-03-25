88ce9c8185e6dcfdbd4d8ea5d4a2b259c4ee7e77
basemkasem <basem.kasem16@gmail.com>
2024-03-25 09:28:05 +0200

    Add a CalcualtorLibary class library

--- Calculator.basemkasem/Program.cs
+++ Calculator.basemkasem/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Text.RegularExpressions;
+﻿namespace CalculatorProgram;
+
+using CalculatorLibrary;
+using System.Text.RegularExpressions;
 class Program
 {
     static void Main(string[] args)
@@ -80,36 +83,5 @@ class Program
         return;
     }
 }
-class Calculator
-{
-    public static double DoOperation(double num1, double num2, string op)
-    {
-        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
 
-        // Use a switch statement to do the math.
-        switch (op)
-        {
-            case "a":
-                result = num1 + num2;
-                break;
-            case "s":
-                result = num1 - num2;
-                break;
-            case "m":
-                result = num1 * num2;
-                break;
-            case "d":
-                // Ask the user to enter a non-zero divisor.
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-                break;
-            // Return text for an incorrect option entry.
-            default:
-                break;
-        }
-        return result;
-    }
-}
 
